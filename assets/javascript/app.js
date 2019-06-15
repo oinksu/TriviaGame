fb50b63eaf96eee19afb5353a42a328f1aefdd0b
sinid <49916315+oinksu@users.noreply.github.com>
2019-06-15 08:21:14 -0400

    added odded options variable array  with questions answers and image addressing

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+var options = [
+    {
+    question: "Who lives in a barrel?", 
+    choice: ["El Chavo", "Doña Florinda", "Don Ramon", "Kiko"],
+    answer: 0,
+    photo: "assets/images/Chavo.jpg"
+    },
+    
+    {
+    question: "His mom always spoils him with a giant ball?", 
+    choice: ["La Chilindrina", "Don Ramon", "Kiko", "El Chavo"],
+    answer: 2,
+    photo: "assets/images/Kiko.gif"
+    },
+     
+    {
+    question: "Who is Don Ramon's child?", 
+    choice: ["EL Chavo", "Kiko", "La Chilindrina", "Profesor Jirafales" ],
+    answer: 2,
+    photo: "assets/images/Chilindrina.jpg"
+    },
+     
+    {
+    question: "Who's Doña Florinda's love interest?", 
+    choice: ["Don Ramon", "El Chavo", "Kiko", "Profesor Jirafales" ],
+    answer: 3,
+    photo: "assets/images/Profesor_Jirafales.jpg"
+    },
+     
+    {
+    question: "Who does Don Ramon hide from paying the rent?", 
+    choice: ["Señor Barriga", "Doña Florinda", "Kiko", "Profesor Jirafales" ],
+    answer: 0,
+    photo: "assets/images/Señor_Barriga.jpg"
+    },
+     
+    {
+    question: "Who is always wrongfully accusing and hitting Don Ramon?", 
+    choice: ["Señor Barriga", "Doña Florinda", "Kiko", "Profesor Jirafales" ],
+    answer: 1,
+    photo: "assets/images/Dona_Florinda.jpg"
+    },
+     
+    {
+    question: "Who is La Bruja del 71 obsessed with?", 
+    choice: ["El Chavo", "Doña Florinda", "Don Ramon", "Kiko" ],
+    answer: 2,
+    photo: "assets/images/Don_Ramon.jpg"
+    },
+     
+    {
+    question: "Who does el Chapulin Colorado look like?", 
+    choice: ["Kiko", "Doña Florinda", "Don Ramon", "El Chavo" ],
+    answer: 0,
+    photo: "assets/images/Chavo.jpg"
+    }];
