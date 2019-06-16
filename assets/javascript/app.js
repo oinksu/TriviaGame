a735d8c0ac9ac68696f97a9758ae63e06a9533c9
sinid <49916315+oinksu@users.noreply.github.com>
2019-06-16 06:45:13 -0400

    added timer elements to app.js sheet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    
     var options = [
     {
     question: "Who lives in a barrel?", 
@@ -52,21 +53,142 @@ $(document).ready(function () {
     {
     question: "Who does el Chapulin Colorado look like?", 
     choice: ["Kiko", "Doña Florinda", "Don Ramon", "El Chavo" ],
-    answer: 0,
+    answer: 3,
     photo: "assets/images/Chavo.jpg"
     }];
     
-    var correctCount = 0;
-    var wrongCount = 0;
-    var unanswerCount = 0;
-    var timer = 20;
-    var intervalId;
-    var userGuess ="";
+    var right = 0;
+    var wrong = 0;
+    var unanswer = 0;
     var running = false;
     var qCount = options.length;
     var pick;
     var index;
     var newArray = [];
     var holder = [];
+    var timer = 20;
+    var intervalId;
+    var userGuess ="";
+
+    $("#reset").hide();
+    
+    //start button to start game
+    $("#start").on("click", function () {
+    $("#start").hide();
+    displayQuestion();
+    runTimer();
+    
+    for(var i = 0; i < options.length; i++) {
+    holder.push(options[i]);
+    }
+    })
+    
+    //timer start
+    function runTimer(){
+    if (!running) {
+    intervalId = setInterval(decrement, 1000); 
+    running = true;
+    }
+    }
+    
+    //timer countdown
+    function decrement() {
+    $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+    timer --;
+    
+    //stop timer if reach 0
+    if (timer === 0) {
+    unanswer++;
+    stop();
+    $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+    hidepicture();
+    }   
+    }
+    
+    //timer stop
+    function stop() {
+    running = false;
+    clearInterval(intervalId);
+    }
+    
+    //rando pick
+    function displayQuestion() {
+    
+    //random index in array
+    index = Math.floor(Math.random()*options.length);
+    pick = options[index];
     
    
+    
+    //iterate answers array shows
+    $("#questionblock").html("<h2>" + pick.question + "</h2>");
+    for(var i = 0; i < pick.choice.length; i++) {
+    var userChoice = $("<div>");
+    userChoice.addClass("answerchoice");
+    userChoice.html(pick.choice[i]);
+    
+    //assign array position to it so can check answer
+    userChoice.attr("data-guessvalue", i);
+    $("#answerblock").append(userChoice);
+    //      }
+    }
+    
+    //click function to select answer and outcomes
+    $(".answerchoice").on("click", function () {
+    //grab array position from userGuess
+    userGuess = parseInt($(this).attr("data-guessvalue"));
+    
+    //correct guess or wrong guess outcomes
+    if (userGuess === pick.answer) {
+    stop();
+    right++;
+    userGuess="";
+    $("#answerblock").html("<p>Correct!</p>");
+    hidepicture();
+    } else {
+    stop();
+    wrong++;
+    userGuess="";
+    $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+    hidepicture();
+    }
+    })
+    }
+    
+    function hidepicture () {
+    $("#answerblock").append("<img src=" + pick.photo + ">");
+    newArray.push(pick);
+    options.splice(index,1);
+    var hidpic = setTimeout(function() {
+    $("#answerblock").empty();
+    timer= 20;
+    
+    //run the score screen if all questions answered
+    if ((wrong + right + unanswer) === qCount) {
+    $("#questionblock").empty();
+    $("#questionblock").html("<h3>Game Over! Here's how you did: </h3>");
+    $("#answerblock").append("<h4> Correct: " + right + "</h4>" );
+    $("#answerblock").append("<h4> Incorrect: " + wrong + "</h4>" );
+    $("#answerblock").append("<h4> Unanswered: " + unanswer + "</h4>" );
+    $("#reset").show();
+    right = 0;
+    wrong = 0;
+    unanswer = 0;
+    } else {
+    runTimer();
+    displayQuestion();
+    }
+    }, 3000);
+    }
+    $("#reset").on("click", function() {
+    $("#reset").hide();
+    $("#answerblock").empty();
+    $("#questionblock").empty();
+    for(var i = 0; i < holder.length; i++) {
+    options.push(holder[i]);
+    }
+    runTimer();
+    displayQuestion();
+    })
+    })
+    
